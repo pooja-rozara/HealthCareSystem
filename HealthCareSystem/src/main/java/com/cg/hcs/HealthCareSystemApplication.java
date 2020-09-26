566e215c52c2092fb1160cc5aa7df24b0ecd243c
Pooja <Pooja@LAPTOP-K8H0O3A5>
2020-09-26 15:39:56 +0530

    commit

--- HealthCareSystem/src/main/java/com/cg/hcs/HealthCareSystemApplication.java
+++ HealthCareSystem/src/main/java/com/cg/hcs/HealthCareSystemApplication.java
@@ -1,9 +1,11 @@
-package com.cg.HealthCareSystem;
+package com.cg.hcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HealthCareSystemApplication {
 
 	public static void main(String[] args) {
--- HealthCareSystem/src/test/java/com/cg/hcs/HealthCareSystemApplicationTests.java
+++ HealthCareSystem/src/test/java/com/cg/hcs/HealthCareSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cg.HealthCareSystem;
+package com.cg.hcs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
